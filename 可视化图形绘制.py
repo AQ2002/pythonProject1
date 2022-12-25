358a731d14e2696c0d90fd0886e38a802f270401
AL2002 <1877389662@qq.com>
2022-12-25 23:10:10 +0800

    2

--- 可视化图形绘制.py
+++ 可视化图形绘制.py
@@ -4,7 +4,7 @@ from pylab import mpl
 frame=pd.read_csv('股票数据.csv',encoding='GBK')
 frame=frame.set_index('日期')
 frame.index=pd.to_datetime(frame.index)
-
+plt.style.use('dark_background')
 plt.scatter(frame['开盘价'],frame['收盘价'],c=frame['成交量'],s=frame['换手率'],alpha=0.5)
 plt.colorbar()
 plt.show()
